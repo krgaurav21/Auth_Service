eb2eb996ea15a52bbf4674d2645d4dbb623e6ade
Gaurav <rajkumargaurav2112@gmail.com>
2023-11-02 08:54:36 +0530

    First Commit: Project Skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const {PORT} = require('./config/serverConfig');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+    app.listen(3001,() => {
+        console.log(`Server Started on port : ${PORT}` );
+    });
+}
+
+
+prepareAndStartServer();
+
+
+
+
+
