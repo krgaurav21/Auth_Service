ddc032bff454dae10adefa1226f73124bbe9d8dc
Gaurav <rajkumargaurav2112@gmail.com>
2023-11-03 16:46:32 +0530

    Added token gen logic from sign in

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const UserController = require('../../controllers/user-controller');
 
+
 const router = express.Router();
 
 router.post('/signup', UserController.create);
+router.post('/signin', UserController.signIn);
+
 
 module.exports = router;
