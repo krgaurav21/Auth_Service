35e71b154a8214473422e6427a3d54b219447b29
Gaurav <rajkumargaurav2112@gmail.com>
2023-11-03 14:58:43 +0530

    Added get all by id in repo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routers/index');  
 
 // const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const UserRepository = require('./repository/user-repository');
 
 const app = express();
 
@@ -18,6 +19,9 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        const repo = new UserRepository();
+        const response = await repo.getById(1);
+        console.log(response);
         // const incomingpassword = '123456';
         // const user = await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
