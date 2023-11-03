571819a775ec8c3d29afa727505c36332b8c55d1
Gaurav <rajkumargaurav2112@gmail.com>
2023-11-03 15:55:05 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routers/index');  
 
-// const {User} = require('./models/index');
-const bcrypt = require('bcrypt');
-const UserRepository = require('./repository/user-repository');
+// const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -19,13 +17,16 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        const repo = new UserRepository();
-        const response = await repo.getById(1);
-        console.log(response);
-        // const incomingpassword = '123456';
-        // const user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
+
+        // const service = new UserService();
+        // const newToken = service.createToken({email: 'sanket@admin.com', id: 1});
+        // console.log("new token is", newToken);
+        // const token =  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjk5MDA2NzIxLCJleHAiOjE2OTkwOTMxMjF9.KB-ShyKyZ4eoCZWLKTsFbxYxgDG-BB0PEqfCquNB_LA
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjk5MDA2ODkzLCJleHAiOjE2OTkwOTMyOTN9.OR6UYW8L66ZZS5lh351D8pbgKwJfLwhW6eHFCcF1M3M';
+
+        // const response = service.verifyToken(token);
         // console.log(response);
+        
     });
 }   
 
